285592e698eed9654d818bbf3a22d542ba703e78
MeltedGreenVelvet <89157503+MeltedGreenVelvet@users.noreply.github.com>
2022-03-31 18:52:54 -0400

    Update validate.js

--- validate.js
+++ validate.js
@@ -12,6 +12,29 @@ const setError = (element, message) => {
     const errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
-    inputControl.classList.remove('success');}
+    inputControl.classList.remove('success');
 }
 
+const setSuccess = element => {
+    const inputControl = element.parentElementl
+    const errorDisplay= input.querySelector('.error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.add('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateEmail = () => {
+    const emailValue = email.value.trim();
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+}
